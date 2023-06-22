48ea1042e1a5a9a9566e8deefc9c2b1a0549b246
Yakovenko Oleksii <93091915+Yakvenalex@users.noreply.github.com>
2023-06-22 14:03:49 +0300

    Update main.py
    444

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
-
+import os
+import asyncio
 
 # Создание объекта клиента
 api_id = 22802758
@@ -10,18 +11,26 @@ client = Client('my_bot', api_id, api_hash)
 save_folder = 'audio/'
 
 # Обработчик новых аудиофайлов и голосовых сообщений
-@client.on_message(filters.audio | filters.voice)
-def handle_media(client, message):
+@client.on_message(filters.audio)
+async def handle_media(client, message):
     # Определение типа файла (аудио или голосовое сообщение)
     if message.audio:
         file_name = 'audio.mp3'
-            
-        # Сохранение файла в указанную папку
+        print(f'Файл "{file_name}" Обнаружен! Приступаю к сохранению.')
+
+        # Сохранение файла в указанную папку в асинхронном режиме
         file_path = save_folder + file_name
-        message.download(file_path)
+        await message.download(file_path)
         print(f'Файл "{file_name}" сохранен.')
 
 
 # Запуск клиента
 if __name__ == '__main__':
-    client.run()
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(client.run())
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.run_until_complete(client.stop())
+        loop.close()
