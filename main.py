518b87864551a5e048337e7ead31ff279fd785ef
Yakovenko Oleksii <93091915+Yakvenalex@users.noreply.github.com>
2023-06-22 12:08:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from pyrogram import Client, filters
+
+
+# Создание объекта клиента
+api_id = 22802758
+api_hash = 'aa30ff3d5301ee9fe860d04349fd5e18'
+client = Client('my_bot', api_id, api_hash)
+
+# Путь к папке для сохранения файлов
+save_folder = 'audio/'
+
+# Обработчик новых аудиофайлов и голосовых сообщений
+@client.on_message(filters.audio | filters.voice)
+def handle_media(client, message):
+    # Определение типа файла (аудио или голосовое сообщение)
+    if message.audio:
+        file_name = 'audio.mp3'
+    elif message.voice:
+        file_name = 'voice.ogg'
+
+    # Сохранение файла в указанную папку
+    file_path = save_folder + file_name
+    message.download(file_path)
+    print(f'Файл "{file_name}" сохранен.')
+
+
+# Запуск клиента
+if __name__ == '__main__':
+    client.run()
